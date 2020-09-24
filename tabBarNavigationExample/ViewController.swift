9a8fbf920637e0964169bd2a112494102d7d6dbf
Julian Rich <s3407957@student.rmit.edu.au>
2020-09-24 14:55:37 +1000

    Initial Commit

---
+++ tabBarNavigationExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabBarNavigationExample
+//
+//  Created by Julian Rich on 24/9/20.
+//  Copyright © 2020 Julian Rich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
